d2f0e4de89b9d651c8c5b9207a5a73f59cf2144b
SamiraSouza07 <samira.campos@germinare.org.br>
2024-02-22 13:25:14 -0300

    Subindo aula 11 e alteração da aula 10

--- Aula11/index.js
+++ Aula11/index.js
@@ -1,19 +1,23 @@
 //getElementsByTagName() - pergar elementos pela tag
 //referencia para a lista não ordenada
 const lista = document.getElementsByTagName("ul")[0];
-const listaTerefas = ["pão", "café", "sorvete"];
+const listaTerefas = [];
 setTimeout(() => {
   menu();
 }, 500);
 //funcao meno
 function menu() {
-  const escolha = window.prompt(`
+  const escolha = window
+    .prompt(
+      `
     [0] - Sair
     [1] - Adicionar tarefa
     [2] - Concluir tarefa
     [3] - Retirar tarefa
     [4] - Editar tarefa
-    `);
+    `
+    )
+    .trim();
   if (escolha == 0) {
     return;
   } else {
@@ -48,7 +52,7 @@ function menu() {
 }
 //função adicionar tarefa
 function adcTarefa() {
-  const tarefa = window.prompt(`adc-Digite a descrição da tarefa: `);
+  const tarefa = window.prompt(`adc-Digite a descrição da tarefa`).trim();
   //Criando o LI
   const li = document.createElement("li");
 
@@ -63,22 +67,21 @@ function adcTarefa() {
 
   const perguntar = window.confirm("adc-Deseja inserir mais tarefas?");
   if (perguntar) {
-    setTimeout(() => {
-      adcTarefa();
-    }, 500);
+    adcTarefa();
   }
 }
 
 //função para concluir a tarefa
 function cclTarefa() {
   //pedindo a tarefa para o usuário
-  const tarefa = window.prompt("ccl-Digite a tarefa a ser concluida");
+  const tarefa = window.prompt("ccl-Digite a tarefa a ser concluida").trim();
 
   //percorrendo a lista de nós
   for (let i = 0; i < listaTerefas.length; i++) {
     if (listaTerefas[i].innerText == tarefa) {
-      //remoção do html
+      //remoção do html e do js
       listaTerefas[i].remove();
+      listaTerefas.splice(i, 1);
 
       //criar nova tarefa riscada
       const li = document.createElement("li");
@@ -90,38 +93,54 @@ function cclTarefa() {
       //adicionando o del no li e o li no ul
       li.appendChild(del);
       lista.appendChild(li);
+
+      //adicionando o elemento li na lista
+      listaTerefas.push(li);
     }
   }
   const perguntar = window.confirm("ccl-Deseja concluir mais tarefas?");
   if (perguntar) {
-    setTimeout(() => {
-      cclTarefa();
-    }, 500);
+    cclTarefa();
   }
 }
 
 //função para retirar tarefa
 function rtrTarefa() {
   //pedindo a tarefa para o usuário
-  const tarefa = window.prompt("rtr-Digite a tarefa a ser retirada");
+  const tarefa = window.prompt("rtr-Digite a tarefa a ser retirada").trim();
 
   //percorrendo a lista de nós
   for (let i = 0; i < listaTerefas.length; i++) {
     if (listaTerefas[i].innerText == tarefa) {
       //remoção do html
       listaTerefas[i].remove();
+      listaTerefas.splice(i, 1);
     }
   }
 
   const perguntar = window.confirm("rtr-Deseja retirar mais tarefas?");
   if (perguntar) {
-    setTimeout(() => {
-      rtrTarefa();
-    }, 500);
+    rtrTarefa();
   }
 }
 
 //função para editar tarefa
 function edtTarefa() {
+  //pedindo a tarefa para o usuário
+  const tarefa = window.prompt("edt-Digite a tarefa a ser editada").trim();
+  //percorrendo a lista de nós
+  for (let i = 0; i < listaTerefas.length; i++) {
+    if (listaTerefas[i].innerText == tarefa) {
+      //pedindo a alteração
+      const alteracao = window.prompt("edt-Digite a nova tarefa");
 
+      //remoção do html
+      listaTerefas[i].innerText = alteracao;
+    }
+  }
+
+  const perguntar = window.confirm("rtr-Deseja editar mais tarefas?");
+  if (perguntar) {
+    edtTarefa();
+  }
 }
