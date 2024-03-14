ddc925239611acb0aa9d25c4b852e8a694bc711a
SamiraSouza07 <samira.campos@germinare.org.br>
2024-03-14 15:52:36 -0300

    Subindo alterações da aula 18

--- aula18/index.js
+++ aula18/index.js
@@ -1,30 +1,41 @@
-const ul = document.getElementById('lista-tarefas')
-// let cont = 0;
-// const body = document.getElementsByTagName("body")[0];
-// function clicar() {
-//   const img = document.createElement("img");
-//   img.setAttribute("src", "pipi.jpg");
-//   img.classList.add("img");
-//   img.setAttribute("style", "margin:5px");
-//   body.appendChild(img);
-//   cont += 1;
-// }
-// function reset() {
-//     let imgs = document.getElementsByClassName("img")[cont-1];
-//     body.removeChild(imgs);
-//     cont--;
+const div = document.getElementById('bloco-tarefas')
+let qntTarefas = 0;
+let cont = 0;
+const div2 = document.getElementById('bloco-imagens')
+function clicar() {
+  const img = document.createElement("img");
+  img.setAttribute("src", "pipi.jpg");
+  img.classList.add("img");
+  div2.appendChild(img);
+  cont += 1;
+}
+function reset() {
+    let imgs = document.getElementsByClassName("img")[cont-1];
+    div2.removeChild(imgs);
+    cont--;
 
-// }
+}
 
 function criarTarefa() {
-  const input = document.getElementById("tarefa")
+  const input = document.getElementById("tarefa");
   const conteudoTarefa = input.value;
-  input.value=""
-  
-  //criando elemento de tarefa
-  const li = document.createElement('li')
-  li.textContent = conteudoTarefa
-  li.classList.add("tarefas-style")
-  ul.appendChild(li)
-  
+  input.value = "";
+
+  if (qntTarefas == 0) {
+    const ul = document.createElement("ul");
+    ul.setAttribute("id","lista-tarefas")
+    const li = document.createElement("li");
+    li.textContent = conteudoTarefa;
+    li.classList.add("tarefas-style");
+    ul.appendChild(li);
+    div.appendChild(ul)
+    qntTarefas+=1
+  } else {
+    const ul = document.getElementById('lista-tarefas');
+    const li = document.createElement("li");
+    li.textContent = conteudoTarefa;
+    li.classList.add("tarefas-style");
+    ul.appendChild(li);
+    qntTarefas+=1
+  }
 }
