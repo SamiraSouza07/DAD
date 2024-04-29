69dcfbe2d71e7471bfec2ea326d7f85df455c2c2
SamiraSouza07 <samira.campos@germinare.org.br>
2024-04-29 18:17:34 -0300

    Subindo alterações da imagem docker caça letras

--- imagens/caca_letras/jogo/2.1-tela-tutorial/index.js
+++
@@ -1,7 +0,0 @@
-const voltar = document.getElementById("voltar")
-
-voltar.addEventListener("click",(e)=>{
-    if(e.target == voltar){
-        window.location.href = "../index.html"
-    }
-})
--- imagens/caca_letras/jogo/4-tela-ganhou-perdeu/index.js
+++
@@ -1,4 +0,0 @@
-const bt = document.getElementById("inicio")
-bt.addEventListener("click",()=>{
-    window.location.href = "../index.html"
-})
