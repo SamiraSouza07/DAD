8bb30a74b55574703f39d3a6ed3ca2b4f028bfcb
SamiraSouza07 <samira.campos@germinare.org.br>
2024-08-06 12:06:02 -0300

    subindo 0508

---
+++ aulas/0508/index.js
@@ -0,0 +1,5 @@
+//const matematica = require("./matematica")
+import { soma, subtracao } from "./matematica.js";
+console.log("Ola mundo");
+console.log(soma(1, 3));
+console.log(subtracao(3, 2));
---
+++ aulas/0508/matematica.js
@@ -0,0 +1,5 @@
+export const soma = (a, b) => a + b;
+export const subtracao = (a, b) => a - b;
+
+//module.exports = { soma, subtracao };
+// export default { soma, subtracao };
